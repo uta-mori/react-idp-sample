7ea47cb39f2a2be193799e413fb224e439fecfdc
uta-mori <mori_yuta@retail-ai.jp>
2020-12-06 23:59:26 +0900

    💄  tailwind導入

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,14 +1,13 @@
-import '../styles/globals.css'
-import React, { useEffect } from 'react'
-import { AppProps } from 'next/app'
-import { AuthProvider } from '../context/Auth'
+import { AppProps } from "next/app";
+import { AuthProvider } from "../context/Auth";
+import "tailwindcss/tailwind.css";
 
 const MyApp = ({ Component, pageProps }: AppProps): JSX.Element => {
   return (
-     <AuthProvider>
-       <Component {...pageProps} />
-     </AuthProvider>
-  )
-}
+    <AuthProvider>
+      <Component {...pageProps} />
+    </AuthProvider>
+  );
+};
 
-export default MyApp
+export default MyApp;
